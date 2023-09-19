d2f89d25838715139a133338f49cc95c4169b87a
unknown <jasminetam94@gmail.com>
2023-09-19 12:09:22 +0100

    Add project

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//Connect to database
+connectDB();
+
+app.use(cors());
+
+app.use("/graphql", graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === "development"
+}))
+
+
+// app.use(express.static("public"));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public", "index.html"));
+// });
+
+app.listen(port, console.log(`Server running on port ${port}`));
